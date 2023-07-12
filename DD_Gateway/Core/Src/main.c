b5e06965c58e07693b786d4c344a6c0707ebfd34
CLoMg <914657384@qq.com>
2023-07-12 13:48:47 +0800

    道钉Flash测试程序，可正常读写

--- DD_Gateway/Core/Src/main.c
+++ DD_Gateway/Core/Src/main.c
@@ -87,7 +87,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART3_UART_Init();
-  MX_SPI3_Init();
+  MX_SPI1_Init();
   /* USER CODE BEGIN 2 */
   BSP_W25Qx_Init();
   /* USER CODE END 2 */
